c83d57241efcc6c75c012317f0b26cb232803695
Viktor Törnblom <viktors.mobil@gmail.com>
2023-12-19 09:19:14 +0100

    added getList and navigates

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import './Pages/Login.js';
-import './Pages/Home.js';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./Pages/Login.js";
+import Home from "./Pages/Home.js";
 
 function App() {
   return (
     <BrowserRouter>
-    <Links />
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="" element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="*" element={<h2>404 Not Found</h2>} />
-    </Routes>
-  </BrowserRouter>
+      <Links />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<h2>404 Not Found</h2>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
