eed23d954595841d2ae1aff50c1aad1feb2abd80
Viktor Törnblom <viktors.mobil@gmail.com>
2023-12-19 13:07:41 +0100

    made createcustomer

--- crm/src/Pages/CreateCustomer.js
+++ crm/src/Pages/CreateCustomer.js
@@ -1,7 +1,50 @@
 function CreateCustomer() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const company = data.get("company");
+    const date = data.get("date");
+    const status = data.get("status");
+    const details = data.get("details");
+    const contact = data.get("contact");
+    await fetch("http://localhost:5000/create-customer", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ company, date, status, details, contact }),
+    });
+    return await response.json();
+  };
   return (
     <>
-      <h1>Create Appointment</h1>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <label>
+          Company Name:
+          <input type="text" name="company" />
+        </label>
+        <label>
+          Date:
+          <input type="date" name="date" />
+        </label>
+        <label>
+          Status:
+          <select name="status">
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
+            <option value="red">Red</option>
+          </select>
+        </label>
+        <label>
+          Details:
+          <textarea name="details" />
+        </label>
+        <label>
+          Contact:
+          <input type="text" name="contact" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </>
   );
 }
