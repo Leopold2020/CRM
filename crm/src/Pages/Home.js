5fc51c4a68547c9fe2727dc004f57c6e06f85461
Max N <max.vo.nordstrom@gmail.com>
2023-12-19 09:14:33 +0100

    fixed an issue where header

--- crm/src/Pages/Home.js
+++ crm/src/Pages/Home.js
@@ -1,20 +1,22 @@
-export function Home() {
-  const getDate = async () => {
-    await fetch("http://localhost:5000/get-date", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return await response.json();
-  };
+import './Home.css';
+
+function Home() {
+  // const getDate = async () => {
+  //   await fetch("http://localhost:5000/get-date", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   return await response.json();
+  // };
 
   return (
     <>
       <div classname="navbar">
-        <p classname="date">{getDate}</p>
+        {/* <p classname="date">{getDate}</p> */}
         <p classname="list-title">Today's business:</p>
-        <ul>
+        {/* <ul>
           {list.map((item) => (
             <li classname="list-item">
               <p>{item.details}</p>
@@ -34,7 +36,7 @@ export function Home() {
               <button classname="admin-button">Edit</button>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
       <ul classname="footer">
         <li classname="footer-item">stuff</li>
@@ -42,3 +44,5 @@ export function Home() {
     </>
   );
 }
+
+export default Home;
