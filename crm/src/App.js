c74f58c8b3440c35a2ff8969e63ed94eda2b3e0d
Max N <max.vo.nordstrom@gmail.com>
2023-12-19 09:34:53 +0100

    fixed an issue

--- crm/src/App.js
+++ crm/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Login from './Pages/Login.js';
 import Home from './Pages/Home.js';
 import Header from './Components/Header.js';
+import Item from './Pages/Item.js';
+import EditItem from './EditItem.js';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path="login" element={<Login />} />
+      <Route path="item" element={<Item />} />
+      <Route path="edit-item" element={<EditItem />} />
       <Route path="*" element={<h2>404 Not Found</h2>} />
     </Routes>
   </BrowserRouter>
---
+++ crm/src/Pages/Item.js
@@ -0,0 +1,9 @@
+import './Item.css';
+
+function Item() {
+  return(
+    <></>
+  )
+}
+
+export default Item;
